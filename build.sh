9b8b35536428d6426641fc29b710d5097be46806
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-04-04 17:33:12 +0200

    new build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./gradlew clean cE eclipse build install gitChangelogTask || exit 1
-cd violation-comments-to-gitlab-gradle-plugin-example
-
-#
-# Get project id: curl -XGET "https://gitlab.com/api/v4/projects/tomas.bjerre85%2Fviolations-test"
-# MR: https://gitlab.com/api/v4/projects/2732496/merge_requests
-#
-./gradlew violationCommentsToGitLab -DGITLAB_URL=https://gitlab.com/ -DGITLAB_MERGEREQUESTIID=1 -DGITLAB_PROJECTID=tomas.bjerre85/violations-test -DGITLAB_APITOKEN=$GITLAB_APITOKEN -i --stacktrace
